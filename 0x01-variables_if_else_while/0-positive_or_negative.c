96017d7199a79aa3f5aae4427127875d4a0a4d39
root <halimahmaria@>
2022-03-13 00:49:13 -0800

    if else

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,22 +3,24 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
- * Description: prints "is it positive or negative"
- * Return: Always 0 (Sucess)
- */
+* main - Entry code
+* Description: prints "last digit"
+* Return: 0
+*/
 
 int main(void)
 {
 	int n;
+	int i;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n > 0)
-		printf("%d is positive\n", n);
-	else if (n == 0)
-		printf("%d is zero\n", n);
-	else
-		printf("%d is negative\n", n);
+	i = n % 10;
+	if (i > 5)
+		printf("Last digit of %d is %d and is greater than 5\n", n, i);
+	else if (i == 0)
+		printf("Last digit of %d is %d and is 0\n", n, i);
+	else if (i < 6 && i != 0)
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, i);
 	return (0);
 }
