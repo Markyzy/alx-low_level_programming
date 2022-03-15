659be41254f9f3ac49adb8fdb0a06186f5651a03
root <halimahmaria@>
2022-03-15 11:06:32 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -14,7 +14,7 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	    char p[100] = "_putcher\n";
-    printf("%s", p);
+	    char p = "_putcher\n";
+	    printf("%d", p);
     return 0;
 }
