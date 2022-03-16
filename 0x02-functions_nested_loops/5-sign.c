14ccade4a2741a5ba908ca7f4e5cf0df96f1b36c
root <halimahmaria@>
2022-03-16 01:27:50 -0700

    check sign of numbers

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_sign - prints the sign of a number
+ * @n: the character to check
+ * Return: 1 if + else 0 if is 0 and - if -1
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+		printf("+");
+		return (1);
+	else if(n == 0)
+		printf("0");
+	return (0);
+	else if (n < 0)
+		printf("-");
+	return (-1);
