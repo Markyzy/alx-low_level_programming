511cf649ad3a6dcafab1333c766654c32333b23f
root <halimahmaria@>
2022-03-15 21:42:38 -0700

    alphabet 10x

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,13 +5,14 @@
  */
 
 void print_alphabet_x10(void)
+
 {
 	char c = 'a';
 	int i;
 
-	for (c = 'a'; c <= 'z'; c++){
-	for (i = 0; i <= 10; i++)
+	for (c = 'a'; c <= 'z'; c++);
+	for (i = 0; i <= 10; i++);
 	putchar(c);
 	putchar('\n');
 }
-}
+
