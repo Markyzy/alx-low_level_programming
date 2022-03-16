cc9b3ae51dc0d8855cb283c4f537f76b92896dc1
root <halimahmaria@>
2022-03-15 21:50:53 -0700

    alphabet 10x

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,10 +9,10 @@ void print_alphabet_x10(void)
 {
 	char c = 'a';
 	int i = 0;
-
+	
+	for (i = 0; i<=10; i++){
 	for (c = 'a'; c <= 'z'; c++);
-	for (i = 0; i <= 10; i++);
 	putchar(c);
 	putchar('\n');
 }
-
+}
