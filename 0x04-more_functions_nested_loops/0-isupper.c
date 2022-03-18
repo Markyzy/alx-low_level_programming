62b20743377f1f41e2387d30f077f7d7cdbc3d8b
root <halimahmaria@>
2022-03-18 09:51:07 -0700

    last commit

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,18 +2,18 @@
 
 /**
  * _isupper - check if character is uppercase.
- *@c: print int
+ *@c: test value
  * Return: 1 if character is uppercase and 0 if not.
  */
 
 int _isupper(int c)
 {
-if (c >= 'A' && c <= 'Z')
-{
-return (1);
-}
-else
-{
-return (0);
-}
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
