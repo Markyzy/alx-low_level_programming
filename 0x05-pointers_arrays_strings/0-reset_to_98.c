43bb97d151c6c1b94bda9926fbed007933a32f34
root <halimahmaria@>
2022-03-21 01:50:26 -0700

    pointers and arrays and strings

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - takes a pointer and updates the value it points to to 98
+ * @*n : test value the pointer  points to
+ */
+void reset_to_98(int *n)
+{
+	_putchar("Address of variable 'n': %p\n", &p);
+	return (0);
+}
