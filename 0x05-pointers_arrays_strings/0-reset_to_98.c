bfd936bb7f0a9e6de990654517786fd6f47da4d0
root <halimahmaria@>
2022-03-21 02:11:27 -0700

    pointers and arrays and strings

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,7 +3,7 @@
 /**
  * reset_to_98 - function that takes a pointer to an in as a parameter
  * and updates the value it points to to 98
- * @*n : input value
+ * @n : input value
  * Return: n
  */
 void reset_to_98(int *n)
