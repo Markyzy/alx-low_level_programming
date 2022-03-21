f145409e0e83d7b1a06d28208964dde3aa8e9cb9
root <halimahmaria@>
2022-03-21 03:23:14 -0700

    size of the string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+ /**
+  * int_strlen - function that takes char as a parameter
+  * and returns the length of a string
+  * @s : input value
+  * Return : always 0
+  */
+int _strlen(char *s)
+{
+	int c = 1, add =0
+	char p = s[0];
+
+	while (p != '\0')
+	{
+		add++;
+		p = s[c++];
+	}
+	return (add)
+}
