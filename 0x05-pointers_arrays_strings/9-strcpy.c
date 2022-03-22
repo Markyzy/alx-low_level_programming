43cfae0732b6850732675377575f91292f97b01f
root <halimahmaria@>
2022-03-22 12:51:50 -0700

    copy source from to destination

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * char *_strcpy - function that copies the string pointed to by src
+ * @dest: destination copy
+ * @src: source copy
+ * Return: string
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	for (; src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
