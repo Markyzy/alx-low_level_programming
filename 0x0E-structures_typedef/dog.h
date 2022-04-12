64235d03a2acab060fcdccb2b2706f8bf0610e64
root <halimahmaria@>
2022-04-12 12:13:12 -0700

    structs and pre-processor

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,20 +1,23 @@
-#include <stdio.h>
-
+#ifndef _DOG_H_
+#define _DOG_H_
 /**
-  * init_dog - Initializes a dog structure
-  * @d: A dog structure.
-  * @name: The name of the dog.
-  * @age: The age of the dog.
-  * @owner: The owner of the dog.
-  *
-  * Return: Nothing
-  */
-void init_dog(struct dog *d, char *name, float age, char *owner)
+ * struct dog -Dog data.
+ * @name: Name of the dog
+ * @age: age of the dog.
+ * @owner: owner of the dog.
+ * Desctiption: a structure with the name, age and owner of a dog.
+**/
+
+typedef struct dog
 {
-	if (d != NULL)
-	{
-		d->name = name;
-		d->age = age;
-		d->owner = owner;
-	}
-}
+char *name;
+char *owner;
+float age;
+} dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+char *_strncpy(char *dest, char *src, int n);
+int _strlen(char *s);
+#endif
